3d640dab38c74d8c8121a0acd888e3c72031a244
Ronaldo Prata Amorim <roroprata@gmail.com>
2018-11-01 12:36:49 -0200

    Form integration and additional comments

--- app.py
+++ app.py
@@ -5,19 +5,18 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-	return redirect(url_for('info'))
+	return redirect(url_for("drawMol", smiles = ""))
 
+@app.route("/drawMolecule", methods = ['POST', 'GET'])
 @app.route("/drawMolecule/", methods = ['POST', 'GET'])
-def info():
-	if request.method == 'POST':
-		htmlSmiles = request.form['SMILES'].replace("\\", "%5C").replace("/", "%2F")
-		return redirect(url_for('drawMol', smiles=htmlSmiles))
-	return render_template('drawMoleculeForm.html')
-
-@app.route("/drawMolecule/<smiles>/")
-def drawMol(smiles):
-	smiles = smiles.replace("%5C", "\\").replace("%2F", "/")
+@app.route("/drawMolecule/<smiles>", methods = ['POST', 'GET'])
+def drawMol(smiles = ""):
+	#If a SMILES has been submitted through the form.
+	if (request.method == 'POST'):
+		smiles = request.form['SMILES']
 	im = ImageFromSmiles(smiles)
-	if (not im):
-		return "Invalid SMILES"
-	return render_template('drawMolecule.html', smiles = smiles, img=Imageto64(im))
+	#If the smiles is incorrect or is blank.
+	if (not im or smiles == ""):
+		#Draw the page without any images.
+		return render_template('drawMolecule.html', title="SMILES Input")
+	return render_template('drawMolecule.html', title=smiles, img=Imageto64(im))
